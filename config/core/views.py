afa870fe3dda6f9e4c502442617973aa761a361c
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-09-23 01:37:48 -0700

    'index' view added

--- config/core/views.py
+++ config/core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import HttpResponseNotFound
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
+
+
+def index(request):
+    return render(
+        request=request,
+        template_name='index.html'
+    )
+
+
