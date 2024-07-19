4e145bb5b7bc6ae2dccd8f556c79edfcb53ae050
galindo <egalindo54@uabc.edu.mx>
2024-07-18 21:00:37 -0700

    ajustes

---
+++ agenda/form.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Activity
+
+class ActivityForm(forms.ModelForm):
+    class Meta:
+        model = Activity
+        fields = ['name', 'start', 'end']
+        widgets = {
+            'start': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'end': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
--- agenda/views.py
+++ agenda/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+from agenda.form import ActivityForm
 from agenda.models import Activity
-
+    
 
 # Create your views here.
 def calendarView(request):
     return render(request, 'calendar.html', {
-        'form': [a.to_dict() for a in Activity.objects.all()]
+        'form': [a.to_dict() for a in Activity.objects.all()],
+        'activity_form': ActivityForm()
     })
