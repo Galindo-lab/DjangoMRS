5c7de393778e3f29ccf47de92cb2fe6071cbb9fe
galindo <egalindo54@uabc.edu.mx>
2024-07-26 02:52:47 -0700

    links funcionan

--- agenda/views.py
+++ agenda/views.py
@@ -1,23 +1,27 @@
-from django.http import JsonResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import FormView, UpdateView
 
 from agenda.form import EventForm
 from agenda.models import *
 
 
-def agenda_view(request):
-    return render(
-        request=request,
-        template_name='agenda/agenda.html',
-        context={
-            "eventos": [a.to_dict() for a in Event.objects.all()]
-        }
-    )
+class PendingView(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(
+            request=request,
+            template_name='agenda/agenda.html',
+            context={
+                "eventos": [a.to_dict() for a in Event.objects.all()]
+            }
+        )
+
+pending_view = PendingView.as_view()
 
 
-class EventUpdateView(UpdateView):
+class EventUpdateView(LoginRequiredMixin, UpdateView):
     model = Event
     form_class = EventForm
     template_name = 'agenda/event.html'
@@ -27,7 +31,7 @@ class EventUpdateView(UpdateView):
 event_update_view = EventUpdateView.as_view()
 
 
-class EventView(FormView):
+class EventView(LoginRequiredMixin, FormView):
     template_name = 'agenda/event.html'
     success_url = reverse_lazy('index')
     form_class = EventForm
@@ -41,23 +45,3 @@ class EventView(FormView):
 
 
 event_view = EventView.as_view()
-
-
-# Create your views here.
-def calendarView(request):
-    return render(request, 'calendar_alphine.html', {
-        'form': [a.to_dict() for a in Event.objects.all()],
-        # 'activity_form': ActivityForm()
-    })
-
-
-"""
-def item_list(request):
-    items = Event.objects.all()
-    return render(request, 'calendar_alphine.html', {'items': items})
-
-
-def item_list_json(request):
-    items = list(Event.objects.values())
-    return JsonResponse(items, safe=False)
-"""
